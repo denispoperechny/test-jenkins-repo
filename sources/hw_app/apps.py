5eeb24ae6928688ccf9856cda03ba8153498f8aa
Den <void@den.com>
2017-12-29 23:47:30 -0600

    hello world app added

---
+++ sources/hw_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class HwAppConfig(AppConfig):
+    name = 'hw_app'
---
+++ sources/hw_app/views.py
@@ -0,0 +1,10 @@
+
+# Create your views here.
+
+
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.shortcuts import render
+from django.views.generic import TemplateView
+class HomeView(TemplateView):
+    template_name = 'index.html'
